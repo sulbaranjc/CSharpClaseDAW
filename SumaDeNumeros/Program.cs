aac6e3401899e290ebcc0d5fc3f9540825de3c73
Juan Carlos Sulbaram <sulbaranjc@gmail.com>
2024-03-06 11:34:45 +0100

    primer commit

---
+++ SumaDeNumeros/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace SumaDeNumeros;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            int num1, num2, suma;
+            Console.Write("Ingrese el primer número: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese el segundo número: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            suma = num1 + num2;
+            Console.WriteLine("La suma de los números es: " + suma);
+        }
+    }
---
+++ numerosImpares/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace NumerosImpares;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 7;
+            for (int i = 0; i <= n; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i+" es impar");
+                }
+            }
+        }
+    }
